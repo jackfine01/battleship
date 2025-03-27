278ec0b920325004e41cb5c07f5ba7be17e593ac
jack <jackfine01@gmail.com>
2025-03-27 10:33:35 -0400

    added intial ship placement generation mechanics.

--- src/player.js
+++ src/player.js
@@ -3,44 +3,53 @@ function genStart(){
     let arr = [ Math.floor(Math.random()*10) , Math.floor(Math.random()*10) ]
     return arr;
 }
-function genEnd(start, length){
+function genCoordinates(length){
+    let start = genStart();
     let horizontal = null;
     let vertical = null;
-    let directionChance = Math.floor(Math.random()*3)
-    if(directionChance === 1){
+    let directionChance = Math.floor(Math.random()*2)
+
+    if(directionChance === 0){
         horizontal = true;
         vertical = false;
-    } else if(directionChance === 2){
+    } else if(directionChance === 1){
         horizontal = false;
         vertical = true;
-    } else {
+    } else { 
         throw new Error('computer direction error')
     }
+
     if(horizontal&&start[0]+length<9){
-        return [start[0]+length,start[1]];
+        return [start, [start[0]+length,start[1]]];
     } else if(horizontal&&start[0]+length>=9){
-        return [start[0]-length,start[1]];
+        return [start, [start[0]-length,start[1]]];
     } else if(vertical&&start[1]+length<9){
-        return [start[0],start[1]+length];
+        return [start, [start[0],start[1]+length]];
     } else if(vertical&&start[1]+length>=9){
-        return [start[0],start[1]-length];
+        return [start, [start[0],start[1]-length]];
     } else {
         throw new Error('computer direction error')
     }
 }
-
+export {genCoordinates}
 class Player {
-    constructor(){
-        this.playerType =  null;
+    constructor(type){
+        this.playerType =  type;
         this.Gameboard = new Gameboard();
     }
     setComputer(){
+        let caPosition = genCoordinates(5);
+        let bbPosition = genCoordinates(4);
+        let crPosition = genCoordinates(3);
+        let suPosition = genCoordinates(3);
+        let ddPosition= genCoordinates(2);
+
         if(this.playerType = 'computer'){
-            this.Gameboard.placeShip('ca', genStart(), genEnd(start, 5));
-            this.Gameboard.placeShip('bb', genStart(), genEnd(start, 4));
-            this.Gameboard.placeShip('cr', genStart(), genEnd(start, 3));
-            this.Gameboard.placeShip('su', genStart(), genEnd(start, 2));
-            this.Gameboard.placeShip('dd', genStart(), genEnd(start, 1));
+            this.Gameboard.placeShip('ca', caPosition[0], caPosition[1]);
+            this.Gameboard.placeShip('bb', bbPosition[0], bbPosition[1]);
+            this.Gameboard.placeShip('cr', crPosition[0], crPosition[1]);
+            this.Gameboard.placeShip('su', suPosition[0], suPosition[1]);
+            this.Gameboard.placeShip('dd', ddPosition[0], ddPosition[1]);
         }
     }
 }
